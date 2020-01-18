17fc6891b7939f45deb170491d85af0797969514
blackwellweb <P.Blackwell1@uni.brighton.ac.uk>
2020-01-18 09:45:24 +0000

    Setting up JSDoc (100%)

--- src/js/index.js
+++ src/js/index.js
@@ -10,10 +10,21 @@ console.log(`${x} and ${y}`);
 
 
 
+
+
 /* Js doc examples -------------------------------------------------*/
 /*------------------------------------------------------------------*/
 /*------------------------------------------------------------------*/
 // @ts-check
+const { add, subtract, divide, multiply } = require('./calculator');
+
+/**
+ * @file index.js is the root file for thios example app
+ * @author Paul Blackwell
+ * @see <a href="https://paulblackwell.dev">Paul Blackwell</a>
+ */
+
+
 
 
 /**
@@ -39,3 +50,83 @@ const todo = {
     id: 1,
     text: 'Hello',
 };
+
+
+
+/**
+ * Calculate Tax
+ * @param {number} amount - Total amount
+ * @param {number} tax - Tax percentage
+ * @returns {string} - Total with a dollar sign
+ */
+const calculateTax = (amount, tax) => {
+    return `$${amount + tax * amount}`;
+}
+
+//console.log(calculateTax(100, 0.1));
+
+
+/**
+ * A student
+ * @typedef {Object} Student
+ * @property {number} id - Student ID
+ * @property {string} name - Student name
+ * @property {string|number} [age] - Student age (optional)
+ * @property {boolean} isActive - Student is active
+ */
+
+
+/**
+ * @type {Student}
+ */
+const student = {
+    id: 1,
+    name: 'John Doe',
+    age: 20,
+    isActive: true
+}
+
+/**
+ * Class to create a person object
+ */
+
+class Person {
+    /**
+     * @param {object} personInfo Information about the person
+     */
+    constructor(personInfo) {
+        /**
+         * @property {string} name  Persons Name
+         */
+        this.name = personInfo.name;
+        /**
+         * @property {string} age Persons age
+         */
+        this.age = personInfo.age;
+    }
+
+    /**
+     * @property {Function} geet A greeting with the name and age
+     * @returns void 
+     */
+    greet() {
+        console.log(`Hello, my name is ${this.name} and I am ${this.age}`);
+    }
+}
+
+
+/**
+ * Person one
+ * See {@link Person}
+ */
+
+const person1 = new Person({
+    name: 'John Doe',
+    age: 30
+});
+//console.log(person1.greet());
+
+
+
+console.log(add(20, 30));
+
---
+++ tutorials/calculator-tutorial.md
@@ -0,0 +1,3 @@
+Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+Quia possimus eaque earum temporibus qui ratione ullam fugiat, a rerum culpa modi,
+facilis laboriosam maxime animi blanditiis, voluptatibus iure voluptates ducimus.
